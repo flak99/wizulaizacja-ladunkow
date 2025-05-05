786f534fc712588cbf741e2784037adc8be0298a
inż. Krystian Flak <flak99@o2.pl>
2025-05-05 18:12:35 +0200

    Poprawiono blad w sytnaxie package.json

--- wizualizacja-ladunkow/src/Components/SideMenu.jsx
+++ wizualizacja-ladunkow/src/Components/SideMenu.jsx
@@ -1,8 +1,3 @@
 export function SideMenu() {
-  return (
-    <>
-      <p className="font-bolt text-4xl underline">Hello</p>
-      <p className="font-bolt text-4xl underline">Hello</p>;
-    </>
-  );
+  return <></>;
 }
